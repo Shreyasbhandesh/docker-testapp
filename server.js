d4daedfd8ca234a18ac1195839daf45bf3659e29
Shreyasbhandesh <shreyasbhandesh@gmail.com>
2025-04-27 20:20:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get("/getUsers", async (req, res) => {
     await client.connect(URL);
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
+    const db = client.db("Shreyas");
+    const data = await db.collection('list').find({}).toArray();
     
     client.close();
     res.send(data);
